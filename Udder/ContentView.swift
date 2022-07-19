8d9280d6b50887ba7cd0000ea2292b690c0f00e5
Iulia Ionascu <iulia.ionascu@zynksoftware.com>
2022-07-19 16:22:55 +0300

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Iulia Ionascu on 19.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Udder")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Iulia Ionascu on 19.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
